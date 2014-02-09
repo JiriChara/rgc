0a29423a1767113c1de7486a2bcda8e70f9a5d77
Jiri Chara <jirik.chara@gmail.com>
2014-02-09 13:47:24 +0100

    Init should configure git

--- lib/rgc/init.rb
+++ lib/rgc/init.rb
@@ -9,16 +9,20 @@ module Rgc
         abort "Cannot read key file #{@key_file}"
       end
 
-      stdout, stderr, status = Open3.capture3("git status  -uno --porcelain")
+      begin
+        stdout, stderr, status = Open3.capture3("git status  -uno --porcelain")
+      rescue Errno::ENOENT
+        abort "Cannot run `git status  -uno --porcelain`."
+      end
 
       unless status.exitstatus == 0
         abort "git status failed - is this a git repository?"
       end
 
-      # unless stdout.length == 0
-      #   $stderr.puts "Working directory not clean"
-      #   abort "Please commit your changes or 'git stash' them."
-      # end
+      unless stdout.length == 0
+        $stderr.puts "Working directory not clean"
+        abort "Please commit your changes or 'git stash' them."
+      end
 
       init_rgc_config_file
 
@@ -29,6 +33,8 @@ module Rgc
           abort "Cannot open .gitattributes for writing."
         end
       end
+
+      add_git_config_options
     end
 
     def init_rgc_config_file
@@ -47,6 +53,20 @@ module Rgc
       rescue
         abort("Invalid rgc config file (#{@config}). Delete it or fix it.")
       end
+    end
+
+    def add_git_config_options
+      smudge = "git config filter.smudge rgc smudge"
+      stdout, stderr, status_smudge = Open3.capture3(smudge)
+
+      clean = "git config filter.clean rgc clean"
+      stdout, stderr, status_clean = Open3.capture3(clean)
+
+      if [0, 0] != [status_smudge, status_clean]
+        abort "Cannot configure git."
+      end
+    rescue Errno::ENOENT
+      abort "Cannot configure git."
     end
   end
 end
